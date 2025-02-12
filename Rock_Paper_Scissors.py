c15e786a3e9f85a10be32b7446d9e56d82d5cda1
NateThunder <n.somevi@hotmail.com>
2025-02-12 09:58:43 +0000

    Update Rock_Paper_Scissors.py
    Updated the draw with case parameter. To print the choice rather than just the letter

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -11,18 +11,24 @@ while True:
         print("Hey buddy, that not a choice!")
         break
     if computer == choice:
-        print(f"I chose {computer} It's a draw. GG\n\n\n")
+        match computer:
+            case "R":
+                print("I chose rock It's a draw. GG\n\n")
+            case "p":
+                print("I chose paper It's a draw. GG\n\n")
+            case "s":
+                print("I chose scissors It's a draw. GG\n\n")                        
     if computer == "R" and choice == "P":
-        print("I chose Rock 🪨\n\nOh no you win\n\n\n")
+        print("I chose Rock 🪨\n\nOh no you win\n\n")
     if computer == "R" and choice == "S":
-        print("I chose Rock 🪨\n\nYaay I win!!!\n\n\n") 
+        print("I chose Rock 🪨\n\nYaay I win!!!\n\n") 
     if computer == "P" and choice == "S":
-        print("I chose Paper 📄\n\nAw nooo, you win!\n\n\n")
+        print("I chose Paper 📄\n\nAw nooo, you win!\n\n")
     if computer == "P" and choice == "R":
-        print("I chose Paper 📄\n\nYaay I am the winner\n\n\n")    
+        print("I chose Paper 📄\n\nYaay I am the winner\n\n")    
     if computer == "S" and choice == "R":
-        print("I chose Scissors ✀\n\nNoo I lose!\n\n\n")
+        print("I chose Scissors ✀\n\nNoo I lose!\n\n")
     if computer == "S" and choice == "P":
-        print("I chose Scissors ✀\n\nI am the winner!!!!\n\n\n")
+        print("I chose Scissors ✀\n\nI am the winner!!!!\n\n")
 
-    print("Cool Cool Cool, lets play again!\n\n\n")
+    print("Cool Cool Cool, lets play again!\n\n")
